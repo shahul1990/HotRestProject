2ccd06aa601b43bd99a52cd3e5440b50d9774448
shahul1990 <shahul1990@gmail.com>
2016-07-23 00:54:02 +0530

    new changes in menu

--- Restaurant/Controllers/HomeController.cs
+++ Restaurant/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Restaurant.Models;
 
 namespace Restaurant.Controllers
 {
     public class HomeController : Controller
     {
+        private RestaurantDBEntities db = new RestaurantDBEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -27,10 +30,38 @@ namespace Restaurant.Controllers
             return View();
         }
 
+        //public ActionResult Menu(int? SelectedCategory)
+        //{
+        //    var categories = db.ItemCategories.OrderBy(q => q.Name).ToList();
+        //    ViewBag.SelectedCategories = new SelectList(categories, "Category_id",  "ItemName", SelectedCategory);
+                
+        //        //departments, "DepartmentID", "Name", SelectedDepartment);
+        //    int categoryId = SelectedCategory.GetValueOrDefault();
+        //    IQueryable<ItemList> itemlist = db.ItemLists.Where(c => !SelectedCategory.HasValue || c.Category_id == categoryId)
+        //        .OrderBy(d => d.id)
+        //        ;
+        //    var sql = itemlist.ToString();
+        //    return View(itemlist.ToList());
+
+
+        //    //IQueryable<Course> courses = db.Courses
+        //    //    .Where(c => !SelectedDepartment.HasValue || c.DepartmentID == departmentID)
+        //    //    .OrderBy(d => d.CourseID)
+        //    //    .Include(d => d.Department);
+        //    //var sql = courses.ToString();
+        //    //return View(courses.ToList());
+        //    //return View();
+        //}
+
+
         public ActionResult Menu()
         {
+            //ViewBag.Message = "Your contact page.";
+
             return View();
         }
+
+
 
         public ActionResult PartyHall()
         {
---
+++ Restaurant/Controllers/MenuController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Restaurant.Models;
+using Restaurant.Repository;
+
+namespace Restaurant.Controllers
+{
+    public class MenuController : Controller
+    {
+        private RestaurantDBEntities db = new RestaurantDBEntities();
+
+        private IProductRepository iProductRepository = new ProductRepository();
+        private ICategoryRepository iCategoryRepository = new CategoryRepository();
+
+        public class ProductsModel
+        {
+            public ProductsModel()
+            {
+                products = new List<ItemList>();
+                categories = new List<SelectListItem>();
+            }
+
+            public List<ItemList> products { get; set; }
+            public List<SelectListItem> categories { get; set; }
+
+            public int CategoryID { get; set; }
+
+        }
+
+
+        // GET: /Menu/
+        public ActionResult Index()
+        {
+            var categories = db.ItemCategories.ToList();
+            return View(categories);
+        }
+
+        public ActionResult Browse(int category)
+        {
+            var categoryModel = db.ItemLists.Include("ItemCategory").All(c=>c.Category_id ==  category);
+            return View(categoryModel);
+        }
+
+        
+        
+    }
+
+}
